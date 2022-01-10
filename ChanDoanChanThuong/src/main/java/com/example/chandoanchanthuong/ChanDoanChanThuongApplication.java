1fcb9b04be866ae7ce787d986dc735b677621899
Sasuke0508 <76610320+Sasuke0508@users.noreply.github.com>
2022-01-10 23:07:50 +0700

    init project
    set up Spring

---
+++ ChanDoanChanThuong/src/main/java/com/example/chandoanchanthuong/ChanDoanChanThuongApplication.java
@@ -0,0 +1,13 @@
+package com.example.chandoanchanthuong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanDoanChanThuongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChanDoanChanThuongApplication.class, args);
+    }
+
+}
---
+++ ChanDoanChanThuong/src/test/java/com/example/chandoanchanthuong/ChanDoanChanThuongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chandoanchanthuong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanDoanChanThuongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
